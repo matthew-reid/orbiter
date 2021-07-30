51aaf027c1580b199b016809004535edc62dcfc7
Matthew Reid <matthewreid87@gmaill.com>
2021-07-30 23:53:35 +1000

    Editor now compiles in 64 bit

--- OVP/D3D7Client/VideoTab.cpp
+++ OVP/D3D7Client/VideoTab.cpp
@@ -411,7 +411,7 @@ INT_PTR CALLBACK VideoTab::AboutDlgProc (HWND hWnd, UINT uMsg, WPARAM wParam, LP
 	switch (uMsg) {
 	case WM_INITDIALOG: {
 		char cbuf[1024];
-		LoadString ((HINSTANCE)GetWindowLongPtr (hWnd, GWL_HINSTANCE), IDS_STRING1, cbuf, 1024);
+		LoadString ((HINSTANCE)GetWindowLongPtr (hWnd, GWLP_HINSTANCE), IDS_STRING1, cbuf, 1024);
 		SetWindowText (GetDlgItem (hWnd, IDC_EDIT1), cbuf);
 		} return TRUE;
 	case WM_COMMAND:
--- Src/Plugin/LuaMFD/LuaMFD.cpp
+++ Src/Plugin/LuaMFD/LuaMFD.cpp
@@ -185,13 +185,13 @@ void ScriptMFD::Update (HDC hDC)
 }
 
 // MFD message parser
-int ScriptMFD::MsgProc (UINT msg, UINT mfd, WPARAM wparam, LPARAM lparam)
+INT_PTR ScriptMFD::MsgProc (UINT msg, UINT mfd, WPARAM wparam, LPARAM lparam)
 {
 	switch (msg) {
 	case OAPI_MSG_MFD_OPENED:
 		// Our new MFD mode has been selected, so we create the MFD and
 		// return a pointer to it.
-		return (int)(new ScriptMFD (LOWORD(wparam), HIWORD(wparam), (VESSEL*)lparam));
+		return (INT_PTR)(new ScriptMFD (LOWORD(wparam), HIWORD(wparam), (VESSEL*)lparam));
 	}
 	return 0;
 }
